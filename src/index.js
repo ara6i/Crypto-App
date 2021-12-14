d6251d2dfdaab3b4762b6772704c287ffc274ff6
ara6i <88967049+ara6i@users.noreply.github.com>
2021-12-15 01:47:03 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./app/store";
 import "antd/dist/antd.css";
 
 ReactDOM.render(
-   <Router>
+  <Router>
       <Provider store={store}>
     <App />
   </Provider>
